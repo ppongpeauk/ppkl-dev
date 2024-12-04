1a4658648baeb5bac4d7dc029c0db2f4589fb7e9
Pete Pongpeauk <kurtsiberg@gmail.com>
2024-12-04 02:38:52 -0500

    feat: updated design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
 
-const mainFont = Sora({ subsets: ["latin"] });
+const mainFont = Sora({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
 	title: "Pete Pongpeauk",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import PROFILE_PICTURE from "@/assets/images/profile.jpg";
 import Image from "next/image";
+import Link from "next/link";
 
 export const runtime = "edge";
 
 export default async function Home() {
 	return (
-		<main className="flex flex-col items-center justify-center pt-16">
-			<div className="lg:max-w-[480px] order-1">
+		<main className="flex flex-col px-4 lg:px-0 lg:flex-row items-center justify-center pt-16 gap-16">
+			<div className="lg:max-w-[480px] order-1 lg:order-2">
 				<div className="flex flex-row items-center justify-between">
 					<h1 className="text-xl font-bold">Hi, I&apos;m Pete.</h1>
 					{/* nav */}
@@ -16,12 +17,8 @@ export default async function Home() {
 					</nav> */}
 				</div>
 				<p className="mt-0">
-					I&apos;m a student currently pursuing a Bachelor of Science
-					in Computer Science at <i>George Mason University.</i>
-				</p>
-				<p>
 					I&apos;m a software developer with a passion for building
-					scalable and efficient systems, actively looking for
+					scalable and efficient systems, actively in search of
 					opportunities that will allow me to help make the world a
 					better place.
 				</p>
@@ -30,12 +27,17 @@ export default async function Home() {
 					platform that let&apos;s college students buy and sell goods
 					and services with other students on campus.
 				</p>
-				{/* <p>
-					You can view all of my projects <a href="/projects">here</a>
-					.
-				</p> */}
 				<p>
-					<i>Last updated: October 20th, 2024</i>
+					I&apos;ve also been leading development on{" "}
+					<Link
+						className="font-bold text-black underline underline-offset-4"
+						href="https://trycirclez.org"
+						target="_blank"
+					>
+						Circlez
+					</Link>
+					, a platform that lets manufacturers and distributors track
+					goals and milestones for their production lines.
 				</p>
 				{/* links */}
 				<section className="flex gap-2">
@@ -53,17 +55,17 @@ export default async function Home() {
 					</a>
 					<a href="mailto:pete@ppkl.dev">Contact</a>
 				</section>
-				<section style={{ paddingTop: "1.25rem" }} className="order-2">
-					<Image
-						src={PROFILE_PICTURE.src}
-						alt="Pete Pongpeauk"
-						width={240}
-						height={240}
-						quality={90}
-						className="object-cover rounded-lg border"
-						priority
-					/>
-				</section>
+			</div>
+			<div className="rounded-lg pt-16 lg:pt-0 overflow-hidden">
+				<Image
+					src={PROFILE_PICTURE.src}
+					alt="Pete Pongpeauk"
+					width={320}
+					height={320}
+					quality={90}
+					className="object-cover aspect-[0.75] h-auto"
+					priority
+				/>
 			</div>
 		</main>
 	);
